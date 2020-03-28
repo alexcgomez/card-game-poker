3b1fa52d0dea3904ad13bfb053b2fbaca1d2ef7d
Alex <alexcaceres93@gmail.com>
2020-03-28 17:39:32 +0100

    Trabajando Escalera Real de color

--- hand.js
+++ hand.js
@@ -1,16 +1,3 @@
-const hands = [
-  "royal_straight_flush",
-  "straight_flush",
-  "four_of_a_kind",
-  "full_house",
-  "flush",
-  "straight",
-  "three_of_a_kind",
-  "two_pair",
-  "one_pair",
-  "hi_card"
-];
-
 function royalStraightFlush(cards) {
   cards.every(card => card.shape != null);
 }
