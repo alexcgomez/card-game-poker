e8cc8fc1cbee367da58f33fcc7ea6160f1b6b3b4
Alex <alexcaceres93@gmail.com>
2020-03-28 17:30:12 +0100

    Definiendo manos

--- hand.js
+++ hand.js
@@ -1,8 +1,36 @@
-export default class Hand{
-    constructor(...cards) {
-        this.cards = cards;
-    }
-    calcScore(){
-        
-    }
+const hands = [
+  "royal_straight_flush",
+  "straight_flush",
+  "four_of_a_kind",
+  "full_house",
+  "flush",
+  "straight",
+  "three_of_a_kind",
+  "two_pair",
+  "one_pair",
+  "hi_card"
+];
+
+function royalStraightFlush(cards) {
+  cards.every(card => card.shape != null);
+}
+
+export default class Hand {
+  constructor(...cards) {
+    this.cards = cards;
+  }
+
+  calcHand() {
+    //Estas funciones devuelven booleano, a partir de ahi, se calcula la puntuación
+    console.log(royalStraightFlush(this.cards));
+    // straightFlush(this.cards);
+    // fourOfaKind(this.cards);
+    // fullHouse(this.cards);
+    // flush(this.cards);
+    // straight(this.cards);
+    // threeOfaKind(this.cards);
+    // twoPair(this.cards);
+    // onePair(this.cards);
+    // highCard(this.cards);
+  }
 }
--- main.js
+++ main.js
@@ -1,18 +1,36 @@
 import Deck from "./deck.js";
 import Hand from "./hand.js";
 import Player from "./player.js";
+import Card from "./card.js";
 
-
-
-
-
-const d = new Deck();
-const h = new Hand(d.deal(),d.deal(),d.deal(),d.deal(),d.deal());
+let d = new Deck();
+const h = new Hand(
+  new Card("♥", "A"),
+  new Card("♥", "K"),
+  new Card("♥", "Q"),
+  new Card("♥", "J"),
+  new Card("♥", "10")
+);
 const p1 = new Player("Alex", h, 1);
 
+// console.log(p1);
+// console.log(p1.hand);
 
-console.log(JSON.stringify(p1));
+// p1.hand.calcHand();
 
+function royalStraightFlush(cards) {
+  // Para escalera Real de color, 2 condiciones: Tener todas el mismo palo, formar una escalera al As.
 
+  // Todas del mismo palo ?
+  if (
+    cards.every(
+      c => c.shape == "♦" || c.shape == "♠" || c.shape == "♣" || c.shape == "♥"
+    )
+  ) {
+    //Forman escalera?
+  }
+}
 
+console.log(p1.hand.cards);
 
+royalStraightFlush(p1.hand.cards);
