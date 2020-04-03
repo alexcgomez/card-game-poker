df11f247908946cb442812510de6064679069a5a
Alex Caceres <alexcaceres93@gmail.com>
2020-04-04 01:43:57 +0200

    Configuración de webpack terminada

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 //import Deck, { value } from "./deck.js";
-const { Deck, value } = require("./deck");
+const { Deck, value } = require("./js/deck");
 //import Hand from "./hand.js";
-const Hand = require("./hand");
+const Hand = require("./js/hand");
 //import Player from "./player.js";
-const Player = require("./player");
+const Player = require("./js/player");
 //import Card from "./card.js";
 
 let d = new Deck();
@@ -11,6 +11,11 @@ const h = new Hand(d.deal(), d.deal(), d.deal(), d.deal(), d.deal());
 const p1 = new Player("Alex", h, 1);
 console.log(p1.hand);
 p1.hand.calcHand();
+
+const greeting = document.createElement("h1");
+greeting.textContent = `Hola que tal`;
+
+document.body.appendChild(greeting);
 
 /*
 
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.js"),
+  /* 
+
+  DEVTOOLS : 
+  
+  webpack-dev-server 
+
+  */
+  devServer: {
+    contentBase: "./dist",
+  },
+  /* 
+
+  PLUGINS : 
+
+  clean-webpack-plugin, html-webpack-plugin 
+  
+  */
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Poker Game",
+      // favicon: path.resolve(__dirname, "/images/favicon.png"),
+      template: path.resolve(__dirname, "src/base.html"),
+      filename: "index.html", // output file
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  /* 
+
+  LOADERS : 
+
+  css-loader , style-loader, file-loader , csv-loader, xml-loader 
+
+  */
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/,
+        use: ["xml-loader"],
+      },
+    ],
+  },
+};
